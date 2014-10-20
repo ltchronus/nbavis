49fa4fed35d6ecc395f228e00e3c85e0ee59749a
xiaoDC <chenglong01020@gmail.com>
2014-10-21 01:27:20 +0800

    update json2event_data.json

--- dev/components/json2echart_data/src/json2echart_data.js
+++ dev/components/json2echart_data/src/json2echart_data.js
@@ -11,11 +11,17 @@ define(function(require, exports, module) {
   	this._jsonData = this._jsonData.game;
   	this._data['@home_team'] = this._jsonData['@home_team'];
   	this._data['@away_team'] = this._jsonData['@away_team'];
-  	this._data['@plays'] = []; //每次比赛的play数据
+  	this._data['@possessions'] = []; //每次比赛的possession
   	//每次比赛event类型数据
   	this._data['@events'] = [
-  		'twopointmiss',   'rebound',	'shootingfoul',	'freethrowmiss'
+  		'twopointmiss',   'rebound',	'shootingfoul',	'freethrowmiss','freethrowmiss',
+  		'twopointmade','turnover','threepointmiss','personalfoul','freethrowmade',
+  		'offensivefoul','teamtimeout','threepointmade','technicalfoul'
   	]; 
+
+    var  period_number = 
+
+  	
 
   	return this._data;
   };
